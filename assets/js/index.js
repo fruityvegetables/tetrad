8f5bdf4f4d5dddcc205261e2f45e7dc7e780d07d
fruityvegetables <gg.troyer@gmail.com>
2021-12-07 19:31:49 -0600

    added some images to the homepage

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,18 @@
+//~~~~~~~~~ validate contact form email
+
+// function validateEmail() {
+//   var emailID = document.contactForm.user_email.value;
+//   atpos = emailID.indexOf("@");
+//   dotpos = emailID.lastIndexOf(".");
+  
+//   if (atpos < 1 || ( dotpos - atpos < 2 )) {
+//      alert("Please enter correct email ID")
+//      document.contactForm.user_email.focus() ;
+//      return false;
+//   }
+//   return( true );
+// }
+
 //~~~~~~~~~ emailJS LOGIC
 
 window.onload = function () {
